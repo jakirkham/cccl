63881389df6d4cc31144141d3188552b41daf373
Bernhard Manfred Gruber <bernhardmgruber@gmail.com>
2025-01-31 18:16:29 +0100

    Fix

--- cub/test/catch2_test_device_reduce.cuh
+++ cub/test/catch2_test_device_reduce.cuh
@@ -114,8 +114,6 @@ CUB_NAMESPACE_END
 
 CUB_NAMESPACE_BEGIN
 
-CUB_NAMESPACE_BEGIN
-
 template <typename Key, typename Value>
 static std::ostream& operator<<(std::ostream& os, const KeyValuePair<Key, Value>& val)
 {
